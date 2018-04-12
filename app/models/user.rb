c70625029fdeb1b88dcf628dd495e156a76ace41
bkozhaev <bkozhaev@gmail.com>
2018-04-12 21:22:05 +0400

    Solution task 49-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,63 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+  validates :email, format: { with: /.+@.+\..+/i }
+  validates :username, length: { maximum: 40 }
+  validates :username, format: { with: /^[a-z0-9_]{1,40}$/}
+
+  before_save :encrypt_password
+
+
+  def encrypt_password
+    if self.password.present?
+      # создаем так называемую "соль" - рандомную строку усложняющую задачу хакерам.
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      # создаем хэш пароля - длинная уникальная строка, из которой невозможно восстанвоить
+      # исходный пароль
+      self.password_hash = User.hash_to_string(
+          OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  # служебный метод, преобразующий бинарную строку в 16-ричный формат, для удобства хранения
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    # Сперва находим кандидата по email
+    user = find_by(email: email)
+
+    # Если пользователь не найдет, возвращаем nil
+    return nil unless user.present?
+
+    # Формируем хэш пароля из того, что передали в метод
+    hashed_password = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+            password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+    )
+
+    # Обратите внимание: сравнивается password_hash, а оригинальный пароль так
+    # никогда и не сохраняется нигде. Если пароли совпали, возвращаем
+    # пользователя.
+    return user if user.password_hash == hashed_password
+
+    # Иначе, возвращаем nil
+    nil
+  end
 end
