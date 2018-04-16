3ede461305352083c9d5c625359ad00cdb7d11e4
bkozhaev <bkozhaev@gmail.com>
2018-04-16 13:33:00 +0400

    added div with number of questions, following sklonenie

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,29 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def sklonenie(number, krokodil, krokodila, krokodilov)
+    if (number == nil | !number.is_a?(Numeric))
+      number	= 0
+    end
+
+    ostatok = number % 10
+    ostatok_sto = number%100
+
+    if (ostatok_sto >= 11 && ostatok_sto <= 14)
+      return krokodilov
+    end
+
+    if (ostatok == 1)
+      return krokodil
+    end
+
+    if (ostatok >= 2 && ostatok <= 4)
+      return krokodila
+    end
+
+    if (ostatok > 4 || ostatok == 0)
+      return krokodilov
+    end
+  end
 end
