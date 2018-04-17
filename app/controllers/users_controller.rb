7a75e6207ec29753bf0943761860b6712ac9fd0a
bkozhaev <bkozhaev@gmail.com>
2018-04-17 12:31:17 +0400

    added actions create and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегестрирован!'
+    end
+  end
+
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
   end
 end
