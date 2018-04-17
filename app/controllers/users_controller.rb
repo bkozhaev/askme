0dcc67f4cb5a3a9e027b39c58202fb28085caf3c
bkozhaev <bkozhaev@gmail.com>
2018-04-17 16:29:55 +0400

    users + sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,52 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :new, :create]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
   def index
    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
 
     if @user.save
       redirect_to root_url, notice: 'Пользователь успешно зарегестрирован!'
+    else
+      render 'new'
     end
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены!'
+    else
+      render 'edit'
+    end
   end
 
   def show
+    @questions = @user.questions.order(created_at: :desc)
+
+    @new_question = @user.questions.build
+  end
+
+  private
+  def authorize_user
+    reject_user unless @user == current_user
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
   root 'users#index'
   resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :questions
 
+
   get 'show' => 'users#show'
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
