8fc0552a6b424e2c6832e5880ae340cbab76559b
bkozhaev <bkozhaev@gmail.com>
2018-05-16 12:40:15 +0400

    added tag to answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,7 +10,11 @@ class Question < ApplicationRecord
 
   after_create do
     question = Question.find_by(id: self.id)
-    hashtags = self.text.scan(/#[\p{L}0-9_]{1,55}/)
+    if self.answer.present?
+      hashtags = self.answer.scan(/#[\p{L}0-9_]{1,55}/)
+    else
+      hashtags = self.text.scan(/#[\p{L}0-9_]{1,55}/)
+    end
     hashtags.uniq.map do |hashtag|
       tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
       question.tags << tag
@@ -20,7 +24,12 @@ class Question < ApplicationRecord
   after_update do
     question = Question.find_by(id: self.id)
     question.tags.clear
-    hashtags = self.text.scan(/#[\p{L}0-9_]{1,55}/)
+    if self.answer.present?
+      hashtags = self.answer.scan(/#[\p{L}0-9_]{1,55}/)
+    else
+      hashtags = self.text.scan(/#[\p{L}0-9_]{1,55}/)
+    end
+
     hashtags.uniq.map do |hashtag|
       tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
       question.tags << tag
