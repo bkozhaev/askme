1517328792a1553bfb3d7c93755a46e352f5faa8
bkozhaev <bkozhaev@gmail.com>
2018-05-31 10:32:20 +0400

    move methods from ApplicationRecord to Tag

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,31 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  def question_tag_create
-    question = Question.find_by(id: self.id)
-    if self.answer.present?
-      hashtags = self.answer.scan(/#[\p{L}0-9_]{1,55}/)
-    else
-      hashtags = self.text.scan(/#[\p{L}0-9_]{1,55}/)
-    end
-    hashtags.uniq.map do |hashtag|
-      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
-      question.tags << tag
-    end
-  end
-
-  def question_tag_update
-    question = Question.find_by(id: self.id)
-    question.tags.clear
-    if self.answer.present?
-      hashtags = self.answer.scan(/#[\p{L}0-9_]{1,55}/)
-    else
-      hashtags = self.text.scan(/#[\p{L}0-9_]{1,55}/)
-    end
-
-    hashtags.uniq.map do |hashtag|
-      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
-      question.tags << tag
-    end
-  end
-
 end
