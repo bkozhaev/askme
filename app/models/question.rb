0f06ff6d84525be6f6ffb4fc7876d7b707dc9a0e
bkozhaev <bkozhaev@gmail.com>
2018-06-04 11:32:11 +0400

    tag code refactoring

--- app/models/question.rb
+++ app/models/question.rb
@@ -15,4 +15,21 @@ class Question < ApplicationRecord
   after_update do
     question_tag_update
   end
+
+  def question_tag_create
+    question = Question.find_by(id: self.id)
+    check_presence.uniq.map do |hashtag|
+      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+      question.tags << tag
+    end
+  end
+
+  def question_tag_update
+    question = Question.find_by(id: self.id)
+    question.tags.clear
+    check_presence.uniq.map do |hashtag|
+      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+      question.tags << tag
+    end
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,31 +2,12 @@ class Tag < ApplicationRecord
 
   has_and_belongs_to_many :questions
 
-  def question_tag_create
-    question = Question.find_by(id: self.id)
-    if self.answer.present?
-      hashtags = self.answer.scan(/#[\p{L}0-9_]{1,55}/)
-    else
-      hashtags = self.text.scan(/#[\p{L}0-9_]{1,55}/)
-    end
-    hashtags.uniq.map do |hashtag|
-      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
-      question.tags << tag
-    end
-  end
 
-  def question_tag_update
-    question = Question.find_by(id: self.id)
-    question.tags.clear
+  def check_presence
     if self.answer.present?
-      hashtags = self.answer.scan(/#[\p{L}0-9_]{1,55}/)
+      self.answer.scan(/#[\p{L}0-9_]{1,55}/)
     else
-      hashtags = self.text.scan(/#[\p{L}0-9_]{1,55}/)
-    end
-
-    hashtags.uniq.map do |hashtag|
-      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
-      question.tags << tag
+      self.text.scan(/#[\p{L}0-9_]{1,55}/)
     end
   end
 end
