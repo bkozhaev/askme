218dab17b483eac8d238290e369d8f123ac83c63
bkozhaev <bkozhaev@gmail.com>
2018-07-19 16:10:52 +0400

    code refactoring

--- app/models/question.rb
+++ app/models/question.rb
@@ -13,13 +13,14 @@ class Question < ApplicationRecord
   def create_tags_from_answer_and_text
       tag = text.scan(/#[\p{L}0-9_]{1,55}/)
       tag << self.answer.scan(/#[\p{L}0-9_]{1,55}/) if self.answer.present?
+      tag
   end
 
   def question_tag_create
     self.tags.clear
     create_tags_from_answer_and_text.uniq.map do |hashtag|
       tag = Tag.find_or_create_by(name: hashtag.to_s.downcase.delete('[]#""'))
-      self.tags << tag
+      self.tags << tag unless tag.nil?
     end
   end
 end
